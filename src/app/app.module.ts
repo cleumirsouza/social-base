81df7e3a29195adf2124e337dd823ff5f4749e88
Cleumir Souza <cleumirsouza@gmail.com>
2018-01-26 01:45:46 -0200

    add - material design components to application
    Signed-off-by: Cleumir Souza <cleumirsouza@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatTableModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-
 
 @NgModule({
   declarations: [
@@ -16,7 +16,13 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatToolbarModule, 
+    MatTableModule,
+    MatIconModule, 
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
